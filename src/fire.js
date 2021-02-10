d3acd40fac12d71fb011f3c7e997f79e9a0a1d12
ghermidon <mihai_olaru@ymail.com>
2021-02-10 08:06:17 +0200

    first commit

---
+++ src/fire.js
@@ -0,0 +1,11 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyBC-2iuFdcWQME2_0P5c35MiJa9aXi2pVU",
+    authDomain: "simplelogin-405ec.firebaseapp.com",
+    projectId: "simplelogin-405ec",
+    storageBucket: "simplelogin-405ec.appspot.com",
+    messagingSenderId: "1043614064825",
+    appId: "1:1043614064825:web:dacd90cbd848872559bb68"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
